6bd2912cbdd6862f9ce2dbd99a97442d9e083e13
paul_andrew <paul_andrew@stewie.lan>
2013-01-08 20:40:04 +0100

    added some packages to default

--- archlinux/post/postinstall.sh
+++ archlinux/post/postinstall.sh
@@ -6,7 +6,7 @@ KEYMAP=Dvorak
 FONT=Lat2-Terminus16
 FONT_MAP=88591_to_uni
 HOOKS="base udev autodetect block filesystems usbinput fsck"
-PACKAGES="xorg xorg-xinit dwm"
+PACKAGES="xorg xorg-xinit dwm git vim"
 
 echo $HOSTNAME > /etc/hostname
 ln -s /usr/share/zoneinfo/Europe/Stockholm /etc/localtime
