1e581bd0e2ed11189434af91a808c35744671115
pandrew <letters@paulnotcom.se>
2013-01-09 10:07:45 +0100

    Reuse of some functions from archblocks

---
+++ archlinux/_lib/core.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+# ------------------------------------------------------------------------
+# archblocks - modular Arch Linux install script
+# ------------------------------------------------------------------------
+# blocks/lib/core.sh - main installer execution sequence
+
+# PREFLIGHT --------------------------------------------------------------
+
+# buckle up
+#set -o errexit
+
+# check if we're in an IO redirect or incorrectly sourced script
+[ ! -f "${0}" ] && echo "Don't run this directly from curl. Save to file first." && exit
+
+# set mount point, temp directory, script values
+MNT=/mnt; TMP=/tmp/archblocks; POSTSCRIPT="/post-chroot.sh"
+
+# get chroot status
+[ -e "${POSTSCRIPT}" ] && INCHROOT=true || INCHROOT=false
+
+# DEFAULT REPOSITORY URL -------------------------------------------------
+# (probably not useful here if initialization script has already used it,
+# but retained here for reference)
+
+_defaultvalue REMOTE https://raw.github.com/altercation/archblocks/master
+
+# DEFAULT CONFIG VALUES --------------------------------------------------
+
+_defaultvalue HOSTNAME archlinux
+_defaultvalue USERSHELL /bin/bash
+_defaultvalue FONT Lat2-Terminus16
+_defaultvalue FONT_MAP 8859-1_to_uni
+_defaultvalue LANGUAGE en_US.UTF-8
+_defaultvalue KEYMAP us
+_defaultvalue TIMEZONE US/Pacific
+_defaultvalue MODULES ""
+_defaultvalue HOOKS "base udev autodetect pata scsi sata filesystems usbinput fsck"
+_defaultvalue KERNEL_PARAMS # "quiet" # set/used in FILESYSTEM,INIT,BOOTLOADER blocks
+_defaultvalue AURHELPER packer
+_defaultvalue INSTALL_DRIVE query # this overrides any default value set in FILESYSTEM block
+_defaultvalue INIT_MODE systemd # systemd vs anything else. Blocks/helpers can check this to confirm systemd use
+
+#TODO: REMOVE THIS #_defaultvalue PRIMARY_BOOTLOADER UEFI # UEFI or BIOS (case insensitive)
+
+# CONFIG VALUES WHICH REMAIN UNDEFAULTED ---------------------------------
+# for reference - these remain unset if not already declared
+# USERNAME, SYSTEMTYPE
+
+# BLOCKS DEFAULTS --------------------------------------------------------
+
+_defaultvalue INSTALL common/install_pacstrap
+_defaultvalue HARDWARE ""
+_defaultvalue TIME common/time_ntp_utc # or, e.g. time_ntp_localtime
+_defaultvalue SETLOCALE common/locale_default
+_defaultvalue HOST common/host_default
+_defaultvalue FILESYSTEM filesystem/mbr_ext4
+_defaultvalue RAMDISK common/ramdisk_default
+_defaultvalue BOOTLOADER bootloader/bios_grub
+_defaultvalue NETWORK network/wired_wireless_default
+#_defaultvalue INIT init/systemd_pure
+#_defaultvalue INIT=init/systemd_coexist
+#_defaultvalue INIT=init/sysvinit_default
+_defaultvalue XORG ""
+_defaultvalue AUDIO ""
+_defaultvalue VIDEO ""
+_defaultvalue SOUND ""
+_defaultvalue POWER ""
+_defaultvalue SENSORS ""
+_defaultvalue DESKTOP ""
+_defaultvalue POSTFLIGHT "common/postflight_rootpass common/postflight_sudouser"
+_defaultvalue APPSETS ""
+_defaultvalue PACKAGES "git"
+_defaultvalue AURPACKAGES "git"
+
+# ARCH PREP & SYSTEM INSTALL (PRE CHROOT) --------------------------------
+if ! $INCHROOT; then
+_initialwarning                 # WARN USER OF IMPENDING DOOM
+_setfont                        # SET FONT FOR PLEASANT INSTALL EXPERIENCE
+_load_efi_modules || true       # ATTEMPT TO LOAD EFIVARS, EVEN IF NOT USING EFI (REQUIRED)
+_loadblock "${FILESYSTEM}"      # LOAD FILESYSTEM (FUNCTIONS AND VARIABLE DECLARATION ONLY)
+_filesystem_pre_baseinstall     # FILESYSTEM CREATION AND CONFIG
+_loadblock "${INSTALL}"         # INSTALL ARCH
+_filesystem_post_baseinstall    # WRITE FSTAB/CRYPTTAB AND ANY OTHER POST INTALL FILESYSTEM CONFIG
+_filesystem_pre_chroot          # PROBABLY UNMOUNT OF BOOT IF INSTALLING UEFI MODE
+_chroot_postscript              # CHROOT AND CONTINUE EXECUTION
+fi
+
+# ARCH CONFIG (POST CHROOT) ----------------------------------------------
+if $INCHROOT; then
+umount /tmp || _anykey "didn't unmount tmp..."
+_loadblock "${FILESYSTEM}"      # LOAD FILESYSTEM FUNCTIONS
+pacman -Sy
+_filesystem_post_chroot         # FILESYSTEM POST-CHROOT CONFIGURATION
+_systemd && _loadblock "common/systemd_default" # PURE SYSTEMD INSTALL
+_loadblock "${SETLOCALE}"       # SET LOCALE
+_loadblock "${TIME}"            # TIME
+_loadblock "${HOST}"            # HOSTNAME
+                                # DAEMONS
+                                # INIT/SYSTEMD
+_loadblock "${NETWORK}"         # NETWORKING
+_loadblock "${AUDIO}"           # AUDIO
+_loadblock "${VIDEO}"           # VIDEO
+_loadblock "${SOUND}"           # SOUND
+_loadblock "${POWER}"           # POWER
+_loadblock "${SENSORS}"         # SENSORS
+#_loadblock "${KERNEL}"         # KERNEL
+_loadblock "${RAMDISK}"         # RAMDISK
+_loadblock "${BOOTLOADER}"      # BOOTLOADER
+_loadblock "${POSTFLIGHT}"      # COMMON POST INSTALL ROUTINES
+_loadblock "${XORG}"            # XORG
+_loadblock "${DESKTOP}"         # DESKTOP/WM/ETC
+_loadblock "${HARDWARE}"        # COMMON POST INSTALL ROUTINES
+_loadblock "${APPSETS}"         # COMMON APPLICATION/UTILITY SETS
+_installpkg ${PACKAGES}
+_installaur ${AURPACKAGES}
+_loadblock "${MR_BOOTSTRAP+common/mr_bootstrap}" # only if MR_BOOTSTRAP set
+fi
+
---
+++ archlinux/_lib/helpers.sh
@@ -0,0 +1,321 @@
+#!/bin/bash
+# ------------------------------------------------------------------------
+# archblocks - modular Arch Linux install script
+# ------------------------------------------------------------------------
+# blocks/lib/helpers.sh - common helper functions
+
+# DEFAULTVALUE -----------------------------------------------------------
+_defaultvalue ()
+{
+# Assign value to a variable in the install script only if unset.
+# Note that *empty* variables that have purposefully been set as empty
+# are not changed.
+#
+# usage:
+#
+# _defaultvalue VARNAME "value if VARNAME is currently unset or empty"
+#
+eval "${1}=\"${!1-${2}}\"";
+}
+
+# SETVALUE ---------------------------------------------------------------
+_setvalue ()
+{
+# Assign a value to a "standard" bash format variable in a config file
+# or script. For example, given a file with path "path/to/file.conf"
+# with a variable defined like this:
+#
+# VARNAME=valuehere
+#
+# the value can be changed using this function:
+#
+# _setvalue newvalue VARNAME "path/to/file.conf"
+#
+valuename="$1" newvalue="$2" filepath="$3";
+sed -i "s+^#\?\(${valuename}\)=.*$+\1=${newvalue}+" "${filepath}";
+}
+
+# COMMENTOUTVALUE --------------------------------------------------------
+_commentoutvalue ()
+{
+# Comment out a value in "standard" bash format. For example, given a
+# file with a variable defined like this:
+#
+# VARNAME=valuehere
+#
+# the value can be commented out to look like this:
+#
+# #VARNAME=valuehere
+#
+# using this function:
+#
+# _commentoutvalue VARNAME "path/to/file.conf"
+#
+valuename="$1" filepath="$2";
+sed -i "s/^\(${valuename}.*\)$/#\1/" "${filepath}";
+}
+
+# UNCOMMENTVALUE ---------------------------------------------------------
+_uncommentvalue ()
+{
+# Uncomment out a value in "standard" bash format. For example, given a
+# file with a commented out variable defined like this:
+#
+# #VARNAME=valuehere
+#
+# the value can be UNcommented out to look like this:
+#
+# VARNAME=valuehere
+#
+# using this function:
+#
+# _uncommentoutvalue VARNAME "path/to/file.conf"
+#
+valuename="$1" filepath="$2";
+sed -i "s/^#\(${valuename}.*\)$/\1/" "${filepath}";
+}
+
+# ADDTOLIST --------------------------------------------------------------
+_addtolistvar ()
+{
+# Add to an existing list format variable (simple space delimited list)
+# such as VARNAME="item1 item2 item3".
+#
+# Handles lists enclosed by either "quotes" or (parentheses)
+#
+# Usage (internal variable)
+# _addtolist "new item" newitem newitem
+#
+if [ "$#" -lt 3 ]; then
+newitem="$1" listname="$2"
+eval "${listname}=\"${!listname} $newitem\""
+else # add to list variable in an existing file
+newitem="$1" listname="$2" filepath="$3";
+sed -i "s_\(${listname}\s*=\s*[^)]*\))_\1 ${newitem})_" "${filepath}";
+sed -i "s_\(${listname}\s*=\s*\"[^\"]*\)\"_\1 ${newitem}\"_" "${filepath}";
+fi
+}
+
+# DAEMONS ADD/REMOVE -----------------------------------------------------
+_daemon ()
+{
+# TODO: make work for systemd
+# add|enable|change disable remove
+#
+# usage:
+# daemon add @ntp
+# daemon disable network
+# daemon remove hwclock
+# daemon remove hwclock network
+#
+! [ -e "/etc/rc.conf" ] && return 0
+ACTION="$1"; shift; DAEMON_LIST="$@"
+for DAEMON_ITEM in $DAEMON_LIST; do
+DAEMON_BASE=$(echo "$DAEMON_ITEM" | sed "s/[!@]*\(.*\)/\1/") # strip any leading characters
+case $ACTION in # assign DAEMON_NEW based on action
+    add|change|enable|on) DAEMON_NEW="$DAEMON_ITEM" ;;
+    disable|off) DAEMON_NEW="!${DAEMON_BASE}" ;; # normalize in case user passes !daemon format as argument
+    remove|delete) DAEMON_NEW="" ;;
+esac
+echo -e "\nTEST: $ACTION $DAEMON_ITEM -> '$DAEMON_NEW'"
+cat /etc/rc.conf | grep DAEMONS
+# process /etc/rc.conf
+if ! egrep -q "^DAEMONS\s*=.*[!@]?${DAEMON_BASE}" /etc/rc.conf; then # no daemon present
+    [ $ACTION != remove ] && sed -i "/^\s*DAEMONS/ s_)_ ${DAEMON_NEW})_" /etc/rc.conf
+else
+    sed -i "/^\s*DAEMONS/ s_[!@]*${DAEMON_BASE}_${DAEMON_NEW}_" /etc/rc.conf
+fi
+# housekeeping: clean up extraneous spaces
+sed -i "/^\s*DAEMONS/ \
+s/  / /g
+s/( /(/g
+s/ )/)/g" /etc/rc.conf
+done
+}
+# convenience functions
+_daemon_add () { _daemon add $@ ; }
+_daemon_enable () { _daemon enable $@ ; }
+_daemon_change () { _daemon change $@ ; }
+_daemon_on () { _daemon on $@ ; }
+_daemon_disable () { _daemon disable $@ ; }
+_daemon_off () { _daemon off $@ ; }
+_daemon_remove () { _daemon remove $@ ; }
+_daemon_delete () { _daemon delete $@ ; }
+
+# ANYKEY -----------------------------------------------------------------
+_anykey ()
+{
+# Provide an alert (with optional custom preliminary message) and pause.
+#
+# Usage:
+# _anykey "optional custom message"
+#
+echo -e "\n$@"; read -sn 1 -p "Any key to continue..."; echo;
+}
+
+
+# INSTALLPKG -------------------------------------------------------------
+_installpkg ()
+{
+# Install package(s) from official repositories, no confirmation needed.
+# Takes single or multiple package names as arguments.
+#
+# Usage:
+# _installpkg pkgname1 [pkgname2] [pkgname3]
+#
+pacman -S --noconfirm "$@";
+}
+
+# INSTALLAUR -------------------------------------------------------------
+_installaur ()
+{
+# Install package(s) from arch user repository, no confirmation needed.
+# Takes single or multiple package names as arguments.
+#
+# Installs default helper first ($AURHELPER)
+#
+# Usage:
+# _installpkg pkgname1 [pkgname2] [pkgname3]
+#
+_defaultvalue AURHELPER packer
+if command -v $AURHELPER >/dev/null 2>&1; then
+    $AURHELPER -S --noconfirm "$@";
+else
+    pkg=$AURHELPER; orig="$(pwd)"; build_dir=/tmp/build/${pkg}; mkdir -p $build_dir; cd $build_dir;
+    for req in wget git jshon; do
+        command -v $req >/dev/null 2>&1 || _installpkg $req;
+    done
+    wget "https://aur.archlinux.org/packages/${pkg:0:2}/${pkg}/${pkg}.tar.gz";
+    tar -xzvf ${pkg}.tar.gz; cd ${pkg};
+    makepkg --asroot -si --noconfirm; cd "$orig"; rm -rf $build_dir;
+    $AURHELPER -S --noconfirm "$@";
+fi;
+}
+
+# CHROOT POSTSCRIPT ------------------------------------------------------
+_chroot_postscript ()
+{
+# handle interactively assigned install drive value
+echo -e "#!/bin/bash\nINSTALL_DRIVE=$INSTALL_DRIVE" > "${MNT}${POSTSCRIPT}";
+grep -v "^\s*INSTALL_DRIVE.*" "${0}" >> "${MNT}${POSTSCRIPT}";
+#cp "${0}" "${MNT}${POSTSCRIPT}";
+chmod a+x "${MNT}${POSTSCRIPT}"; arch-chroot "${MNT}" "${POSTSCRIPT}";
+}
+
+
+# POST INSTALL MESSAGES --------------------------------------------------
+_display_postinstall_messages ()
+{
+echo "\n\nInstallation complete; Reboot and then execute the post-reboot.sh script in the /root directory."
+echo "\n"
+[ -n "${POSTINSTALL_MSGS:-}" ] && echo "${POSTINSTALL_MSGS}"
+}
+_add_postinstall_messags ()
+{
+:
+}
+
+# LOAD BLOCK -------------------------------------------------------------
+_loadblock ()
+{
+[ -z "$@" ] && return
+for _block in $@; do
+isurl=false ispath=false isrootpath=false;
+case "$_block" in
+    *://*) isurl=true ;;
+    /*)    isrootpath=true ;;
+    */*)   ispath=true ;;
+esac
+FILE="${_block/%.sh/}.sh";
+if $isurl; then URL="${FILE}";
+elif [ -f "${DIR/%\//}/${FILE}" ]; then URL="file://${FILE}";
+else URL="${REMOTE/%\//}/archlinux/${FILE}"; fi
+
+_loaded_block="$(curl -fsL ${URL})";
+
+#set +e
+[ -n "$_loaded_block" ] && eval "${_loaded_block}";
+if [ "$?" -gt 0 ]; then
+_anykey "EXECUTION OF BLOCK \"$_block\" EXPERIENCED ERRORS"
+fi
+#set -e
+done
+} 
+
+
+
+# LOAD EFIVARS MODULE ----------------------------------------------------
+_load_efi_modules ()
+{
+# Load efivars (or confirm they've loaded already) and set EFI_MODE for
+# later use by bootloader.
+#
+modprobe efivars || true;
+ls -l /sys/firmware/efi/vars/ &>/dev/null && return 0 || return 1;
+}
+
+# GET UUID ON DRIVE/PARTITION --------------------------------------------
+_get_uuid ()
+{
+# usage:
+# _get_uuid /dev/sda3
+MATCH="$(echo "$1" | sed "s_/_\\\/_g")"
+blkid -c /dev/null | sed -n "/${MATCH}/ s_.*UUID=\"\([^\"]*\).*_\1_p"
+}
+
+# ENABLE REPOSITORIES FOR SPECIFIC LANGUAGES/FRAMEWORKS ------------------
+_enable_haskell_repos ()
+{
+
+# add repos to /etc/pacman.conf
+
+#egrep -q "^\[haskell-testing\]" /etc/pacman.conf || \
+#sed -i '/^\[core\]/i \
+#[haskell-testing]\
+#Server = http://www.kiwilight.com/haskell/testing/$arch\
+#' /etc/pacman.conf
+
+egrep -q "^\[haskell-extra\]" /etc/pacman.conf || \
+sed -i '/^\[core\]/i \
+[haskell-extra]\
+Server = http://archhaskell.mynerdside.com/$repo/$arch\
+' /etc/pacman.conf
+
+egrep -q "^\[haskell\]" /etc/pacman.conf || \
+sed -i '/^\[core\]/i \
+[haskell]\
+Server = http://xsounds.org/~haskell/$arch\
+' /etc/pacman.conf
+
+# update new repos
+
+pacman --noconfirm -Sy
+
+}
+
+# INIT/SYSTEMD FUNCTIONS -------------------------------------------------
+
+# return true if INIT_MODE is systemd (this is the INIT_MODE default)
+_systemd () { [ "$INIT_MODE" == "systemd" ] && return 0 || return 1; }
+
+# MISC FUNCTIONS ---------------------------------------------------------
+
+_setfont () { setfont $FONT; }
+
+# NULL FUNCTIONS (OVERRIDDEN BY EPONYMOUS BLOCKS)-------------------------
+_filesystem_pre_baseinstall () { :; }
+_filesystem_post_baseinstall () { :; }
+_filesystem_pre_chroot () { :; }
+_filesystem_post_chroot () { :; }
+
+
+
+
+
+# CLEANUP BELOW:
+
+#PRIMARY_BOOTLOADER="$(echo "$PRIMARY_BOOTLOADER" | tr [:lower:] [:upper:])";
+#[ "${PRIMARY_BOOTLOADER#U}" == "EFI" ] && _load_efi_modules && EFI_MODE=true || EFI_MODE=false
+
+
+#TODO: should add a first-run (first call to function for each phase) check and initialization phase
