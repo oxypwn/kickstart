ff3241e62c84b24a1c6d5c82cb63573aa4f56ed7
pandrew <letters@paulnotcom.se>
2013-01-22 14:35:11 +0100

    updates

--- archlinux/appsets/xorg_default.sh
+++ archlinux/appsets/xorg_default.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-_installpkg unclutter xorg-xinit xorg dwm dmenu gedit feh scrot simple-scan xpdf mplayer
+_installpkg unclutter xorg-xinit xorg dwm dmenu gedit feh scrot simple-scan unclutter mplayer
 curl -fsL ${REMOTE}/svorak-a5-xkb  -o /usr/share/X11/xkb/symbols/svorak
