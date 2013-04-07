5777a1f102612d8c18764c78625a6aed653faf2d
pandrew <letters@paulnotcom.se>
2013-04-07 13:47:51 +0200

    Fixed typo in xor_default resulting in xfce4-goodies not being installed.

--- archlinux/archlinux/appsets/xorg_default.sh
+++ archlinux/archlinux/appsets/xorg_default.sh
@@ -2,5 +2,5 @@
 
 _installpkg xorg xorg-server xorg-xinit xorg-utils xorg-server-utils xdotool xorg-xlsfonts xclip xorg-xclipboard xscreensaver
 _installpkg firefox chromium feh fontforge scrot simple-scan unclutter mplayer gvfs gvfs-smb udevil
-_installpkg dmenu dwm xfce4 xfce-goodies
+_installpkg dmenu dwm xfce4 xfce4-goodies
 curl -fsL ${REMOTE}/svorak-a5-xkb  -o /usr/share/X11/xkb/symbols/svorak
