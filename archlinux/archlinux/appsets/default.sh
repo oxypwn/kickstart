611c03b117c6574c8a3d2d7c3f4fac8e376910c5
pandrew <letters@paulnotcom.se>
2013-05-07 13:40:36 +0200

    Moved ssh to default appset

--- archlinux/archlinux/appsets/default.sh
+++ archlinux/archlinux/appsets/default.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 _installpkg tmux iptraf-ng
+
+# Install ssh
+_installpkg openssh
+
+
+curl -fsL ${REMOTE}/archlinux/conf/sshd_config -o /etc/ssh/sshd_config
+
+systemctl enable sshd.service
+systemctl enable sshd.socket
+
--- archlinux/archlinux/appsets/ssh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-_installpkg openssh
-
-
-curl -fsL ${REMOTE}/archlinux/conf/sshd_config -o /etc/ssh/sshd_config
-
-systemctl enable sshd.service
-systemctl enable sshd.socket
