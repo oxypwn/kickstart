0293a64dab71165a23b941b4071910dea6b5cf9f
pandrew <letters@paulnotcom.se>
2013-05-26 13:08:29 +0200

    Netcfg to netctl work

--- archlinux/archlinux/post/wired_wireless_default.sh
+++ archlinux/archlinux/post/wired_wireless_default.sh
@@ -9,19 +9,17 @@ _installpkg iproute2
 _installpkg bridge-utils # (optional) - for bridge connections
 _installpkg dialog # (optional) - for the menu based profile and wifi selectors
 _installpkg ifenslave # (optional) - for bond connections
-_installpkg ifplugd # (optional) - for automatic wired connections through net-auto-wired
+_installpkg ifplugd 
 _installpkg wireless_tools # (optional) - for interface renaming through net-rename
-_installpkg wpa_actiond # (optional) - for automatic wireless connections through net-auto-wireless
+_installpkg wpa_actiond 
 _installpkg wpa_supplicant # (optional) - for wireless networking support
 _installpkg iw rfkill
 
-systemctl enable net-auto-wired.service
-systemctl enable net-auto-wireless.service
+#systemctl enable net-auto-wired.service      # deprecated
+#systemctl enable net-auto-wireless.service   # deprecated
 
-_installaur netcfg
-systemctl enable netcfg.service # - uses profiles listed in /etc/conf.d/netcfg NETWORKS=(...)
-systemctl enable netcfg@PROFILENAME.service
-
+systemctl enable netctl-auto@<interface>.service 
+systemctl enable netctl-ifplugd@<interface>.service
 
 cat > /etc/conf.d/netcfg << EOF
 NETWORKS=(last)
@@ -31,7 +29,7 @@ AUTO_PROFILES=("${NETWORK_PROFILE}")
 
 EOF
 
-cat > /etc/network.d/examples/ethernet-static << EOF
+cat > /etc/netctl/examples/ethernet-static << EOF
 CONNECTION='ethernet'
 DESCRIPTION='A basic static ethernet connection using iproute'
 INTERFACE='eth0'
--- archlinux/mercury.ks
+++ archlinux/mercury.ks
@@ -23,7 +23,7 @@ LANGUAGE=en_US.UTF-8
 KEYMAP=svoraka5
 TIMEZONE=Europe/Stockholm
 MODULES="dm_mod dm_crypt aes_x86_64 ext2 ext4 vfat intel_agp drm i915"
-HOOKS="base udev autodetect block usbinput consolefont encrypt filesystems fsck shutdown"
+HOOKS="base udev autodetect block keyboard consolefont encrypt filesystems fsck shutdown"
 # possible fix for occasional blank screen on resume? https://wiki.archlinux.org/index.php/Pm-utils#Blank_screen_issue
 APPSETS="appsets/default"
 NETWORK_PROFILE="ethernet-static"
