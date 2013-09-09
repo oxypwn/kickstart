296a8b5e26adde148fa3af94e1e88adf818f5009
pandrew <letters@paulnotcom.se>
2013-09-09 12:10:00 +0200

    Set hostname from dns.

--- archlinux/archlinux/post/host.sh
+++ archlinux/archlinux/post/host.sh
@@ -2,6 +2,14 @@
 #
 # HOST
 
+# Lookup current ip against dns and if success set HOSTNAME to returned value.
+IPADDR=`hostname -i`
+LOOKUPIP=$(host ${IPADDR})
+if [[ $? = 0 ]] ; then
+    HOSTNAME=$(host `hostname -i` | gawk -F' ' ´{print $5}´)
+fi
+
+
 echo ${HOSTNAME} > /etc/hostname;
 hostname -F /etc/hostname;
 sed -i "s/localhost\.localdomain/${HOSTNAME}/g" /etc/hosts;
